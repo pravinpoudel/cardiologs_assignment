d59755034fbe34fd945de11ab1697c472b985c04
pravinpoudel <prvnpoudel4@gmail.com>
2022-01-01 17:09:36 -0700

    filtering and empty check in multer

--- main_server.js
+++ main_server.js
@@ -4,9 +4,11 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
 const path = require("path");
+const bodyParser = require("body-parser");
 const recordRoute = require("./routes/records");
 
-app.use(express.urlencoded());
+app.use(bodyParser.json());
+
 //setting the statics
 app.use(express.static(path.join(__dirname, "./public/")));
 
@@ -16,9 +18,18 @@ app.use(express.static(path.join(__dirname, "./public/")));
 app.use("/POST/delineation", recordRoute);
 
 app.get("/", (req, res)=>{
-    //determine the content-type and automatically set header for us
+    //determine the content-type and automatically set header for us, awesome !!!
     res.sendFile(path.join(__dirname, "/index.html"));    
 });
+
+app.use((req, res, next)=>{
+    res.status(404).send("Page not found");
+});
+
+//for error handling; error handling middleware
+app.use((err, req, res, next)=>{
+    res.status(500).send(`Sorry there is error with message saying: ${err.message}`);
+})
 
 app.listen(3000, (err)=>{
     if(err){
--- routes/records.js
+++ routes/records.js
@@ -2,86 +2,115 @@
 
 const express = require("express");
 const multer = require("multer");
+const path = require("path");
 const fs = require("fs");
 const readLine = require("readline");
 let router = express.Router();
 
+//we dont have limit on fileSize
 const fileStorageEngine = multer.diskStorage({
     destination: (req, files, cb1) => {
         cb1(null, "./public/records");
     },
     filename: (req, file, cb2) => {
+        //multer will not append file extension for us, so we need to return a filename complete with an extension
         cb2(null, Date.now() + "--" + file.originalname);
     }
 });
 
-const upload = multer({
-    storage: fileStorageEngine
+let upload = multer({
+    storage: fileStorageEngine,
+    fileFilter: (req, file, callback) => {
+        const extension = path.extname(file.originalname).toLowerCase();
+        // choose in between which file should be uplaoded and which should be skipped. Moreover, you can always pass an error
+        //if things go wrong like we do
+        if (extension === ".csv") {
+            callback(null, true);
+        } else {
+            callback(null, false);
+            return callback(new Error("only CSV file allowed"));
+        }
+    }
 });
 
-//error in the multer middleware is handled by express itself
+upload = upload.single("record");
+
+//error in the multer middleware is handled by express itself but if you want to catch the multer error specifically here,
+// check with upload(req, res, (err)=>{}); awesome !!! express documentation on multer end is nice explanation on this !!
+
 router.route("/")
-    .post(upload.single("record"), (req, res) => {
-       const filePath = req.file.path;
-       const results = {
-         P:0, 
-         QRS:0,
-         meanFrequency: 0,
-         minFrequency:{
-         value: 9999999,
-         time: 0
-       },
-       maxFrequency:{
-         value: 0,
-         time: 0
-       }
-         };
+    .post(upload, (req, res) => {
 
-       const frequencyCollector = {sumOfFrequency: 0, cycleCount:0};
+        if (!req.file) {
+            res.status(422).json({
+                message: 'The file is required.'
+            });
+        }
 
-        let time  = 0;
+        if (req.file.size == 0) {
+            res.status(400).send("file is empty");
+        }
+
+        const results = {
+            P: 0,
+            QRS: 0,
+            meanFrequency: 0,
+            minFrequency: {
+                value: 9999999,
+                time: 0
+            },
+            maxFrequency: {
+                value: 0,
+                time: 0
+            }
+        };
+
+        const frequencyCollector = {
+            sumOfFrequency: 0,
+            cycleCount: 0
+        };
+
+        let time = 0;
         let lastTimeStamp = 0;
 
         readLine.createInterface({
-          input: fs.createReadStream(filePath)
-        }).on("line", line=>{
-          const cols = line.split(",");
-          const waveType = cols[0];
-          const startTime = cols[1];
-          const endTime = cols[2];
-          const tags = cols.slice(3);
-          if((waveType === "P" || waveType === "QRS") && tags.includes("premature")){
-            results[waveType]++;
-          }
-          
-          time += (cols[2] - cols[1]);
-          time += (cols[1] - lastTimeStamp);
-          lastTimeStamp = cols[2];
+            input: fs.createReadStream(req.file.path)
+        }).on("line", line => {
+            const cols = line.split(",");
+            const waveType = cols[0];
+            const startTime = cols[1];
+            const endTime = cols[2];
+            const tags = cols.slice(3);
+            if ((waveType === "P" || waveType === "QRS") && tags.includes("premature")) {
+                results[waveType]++;
+            }
+            time += (cols[2] - cols[1]);
+            time += (cols[1] - lastTimeStamp);
+            lastTimeStamp = cols[2];
+            if (waveType === "QRS") {
+                let frequency = Math.floor((60 / time) * 1000);
+                //check if frequency is maxValue or minValue
+                if (frequency > results.maxFrequency.value) {
+                    results.maxFrequency.value = frequency;
+                    results.maxFrequency.time = Number(cols[2]);
+                }
 
-          if(waveType === "QRS"){
-            let frequency = Math.floor((60/time)*1000);
-            //check if frequency is maxValue or minValue
-            if(frequency > results.maxFrequency.value){
-              results.maxFrequency.value = frequency;
-              results.maxFrequency.time = Number(cols[2]); 
+                if (frequency < results.minFrequency.value) {
+                    results.minFrequency.value = frequency;
+                    results.minFrequency.time = Number(cols[2]);
+                }
+
+                frequencyCollector.sumOfFrequency += frequency;
+                frequencyCollector.cycleCount++;
+                time = 0;
             }
 
-            if(frequency<results.minFrequency.value){
-              results.minFrequency.value = frequency;
-              results.minFrequency.time = Number(cols[2]); 
-            }
-
-            frequencyCollector.sumOfFrequency += frequency;
-            frequencyCollector.cycleCount ++;
-            time = 0;
-          }
-
-        }).on("close", ()=>{
-          let heartRate = Math.floor(frequencyCollector.sumOfFrequency/frequencyCollector.cycleCount);
-          results.meanFrequency = heartRate;
-          results.maxFrequency.time += Number(req.body.time);
-          results.minFrequency.time += Number(req.body.time);
-          res.status(200).json(results);
+        }).on("close", () => {
+            let heartRate = Math.floor(frequencyCollector.sumOfFrequency / frequencyCollector.cycleCount);
+            results.meanFrequency = heartRate;
+            results.maxFrequency.time += Number(req.body.time);
+            results.minFrequency.time += Number(req.body.time);
+            res.status(200).json(results);
         });
     });
 
